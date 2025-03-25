add4cc9b43e05be658321767a7a0aac30a354c34
ILFforever <hammymukura@gmail.com>
2025-03-26 01:26:51 +0700

    for vercel

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 // Apply middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
